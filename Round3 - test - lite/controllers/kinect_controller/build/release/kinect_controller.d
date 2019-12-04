34d878a730864f11ca6a57026fab3539aaeabeb6
DMUPraveen <dmu.praveen@gmail.com>
2019-12-04 11:46:05 +0530

    lite version

---
+++ Round3 - test - lite/controllers/kinect_controller/build/release/kinect_controller.d
@@ -0,0 +1,9 @@
+build/release/kinect_controller.o: kinect_controller.c \
+ C:\Users\USER\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\USER\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\USER\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\USER\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\USER\AppData\Local\Programs\Webots/include/controller/c/webots/supervisor.h \
+ C:\Users\USER\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
+ C:\Users\USER\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Users\USER\AppData\Local\Programs\Webots/include/controller/c/webots/range_finder.h
